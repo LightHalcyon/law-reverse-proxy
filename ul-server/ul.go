6aa27ca04a1bd231b6a195f25b8264ce861b00f2
Adityawarman Fanaro <reznov53@live.com>
2019-05-06 13:37:10 +0700

    Fixed method typo

--- ul-server/ul.go
+++ ul-server/ul.go
@@ -132,7 +132,7 @@ func upload(c *gin.Context) {
 
 	c.JSON(http.StatusOK, appError{
 		Code: http.StatusOK,
-		Message:	os.getenv("PROXYURL") + "/download/" + key,
+		Message:	os.Getenv("PROXYURL") + "/download/" + key,
 		// Message: "http://localhost:22006/download/" + key,
 	})
 	return
